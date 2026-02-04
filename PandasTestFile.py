a6a1cf5a3fe7086a208e21a09670b4488151f4a1
ChrisC115 <notebooklm2000@gmail.com>
2026-02-04 19:19:28 +0000

    Day 2

--- PandasTestFile.py
+++ PandasTestFile.py
@@ -2,16 +2,26 @@ import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as mplt 
 
+x = 0
+Cash = 1000
+Bal = 0
 dat = yf.Ticker("AAPL")
 df = pd.DataFrame(dat.history(period='12mo'))
 
 df["ma_10"] = df["Close"].rolling(window=10).mean()
 df["ma_50"] = df["Close"].rolling(window=50).mean()
 
+df = df.dropna()
 
-print(df[["Open","Close","ma_10","ma_50"]].dropna())
+'''Strat Function'''
 
+for i in range(len(df)):
+    if (df["Close"].iloc[i] > df["ma_10"].iloc[i]) & (df["Close"].iloc[i] > df["ma_50"].iloc[i]):
+        x = x + 1
+        print(x)
 
-for _ in len(df):
-    if df["Close"] > df["ma_10"] & df["ma_50"]:
-        print("Hello")
+'''Buy Function'''
+
+def Buy() :
+    Cash = Cash-100
+    Bal = 100
---
+++ pseudo.py
@@ -0,0 +1,35 @@
+'''
+
+Buy Function will spend 100 of cash and set bal = 100 
+
+
+Sell Function will take 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
