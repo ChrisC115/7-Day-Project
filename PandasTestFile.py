dcd7e501f302f39926b9aea1380457d455696818
ChrisC115 <notebooklm2000@gmail.com>
2026-02-04 13:32:55 +0000

    Day 2

--- PandasTestFile.py
+++ PandasTestFile.py
@@ -4,3 +4,8 @@ import matplotlib.pyplot as mplt
 
 dat = yf.Ticker("AAPL")
 print(dat.history(period='12mo'))
+df = pd.DataFrame(dat.history(period='12mo'))
+
+print("\nSimple Day Moving Average 10 Day","\n\n\n",(df.rolling(window=10).mean()))
+
+print("\nSimple Day Moving Average 50 Day","\n\n\n",(df.rolling(window=50).mean()))
