f60e2ac6c6fadb8af984abb264ca31d6e29d1fdb
ChrisC115 <notebooklm2000@gmail.com>
2026-02-04 18:22:36 +0000

    Day 2

--- PandasTestFile.py
+++ PandasTestFile.py
@@ -3,9 +3,15 @@ import yfinance as yf
 import matplotlib.pyplot as mplt 
 
 dat = yf.Ticker("AAPL")
-print(dat.history(period='12mo'))
 df = pd.DataFrame(dat.history(period='12mo'))
 
-print("\nSimple Day Moving Average 10 Day","\n\n\n",(df.rolling(window=10).mean()))
+df["ma_10"] = df["Close"].rolling(window=10).mean()
+df["ma_50"] = df["Close"].rolling(window=50).mean()
 
-print("\nSimple Day Moving Average 50 Day","\n\n\n",(df.rolling(window=50).mean()))
+
+print(df[["Open","Close","ma_10","ma_50"]].dropna())
+
+
+for _ in len(df):
+    if df["Close"] > df["ma_10"] & df["ma_50"]:
+        print("Hello")
