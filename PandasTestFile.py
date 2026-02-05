8dbb69d517b86a99a4873d61f4f715b056b60063
ChrisC115 <notebooklm2000@gmail.com>
2026-02-05 14:37:48 +0000

    Day 3 Complete.

--- PandasTestFile.py
+++ PandasTestFile.py
@@ -2,39 +2,38 @@ import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as mplt 
 
-Cash = 1000
-Profit = 0
-Shares = 0
-buy = 0
-dat = yf.Ticker("AAPL")
-df = pd.DataFrame(dat.history(period='12mo'))
+cash = int(1000)
+profit = int(0)
+shares = int(0)
+buy = bool(False)
+df = yf.download("AAPL", period = "12mo", auto_adjust = False, multi_level_index= False)
 
-df["ma_10"] = df["Close"].rolling(window=10).mean()
-df["ma_50"] = df["Close"].rolling(window=50).mean()
+df["ma_10"] = df["Adj Close"].rolling(window=10).mean()
+df["ma_50"] = df["Adj Close"].rolling(window=50).mean()
 
 df = df.dropna()
 
 '''Strat Function'''
 
-for i in range(len(df)):
-    price = df["Close"].iloc[i]
+for i in range(len(df)-1):
+    price = df["Adj Close"].iloc[i]
     
-    if (price > df["ma_10"].iloc[i]) and (price > df["ma_50"].iloc[i]) and buy == 0:
-        buy = 1
-        Shares = Cash / price 
-        Cash = 0
+    if (price > df["ma_10"].iloc[i]) and (price > df["ma_50"].iloc[i]) and buy == False:
+        buy = bool(True)
+        shares = cash / (df["Open"].iloc[i+1] * 1.001)   #This is for buying on next day to remove same day buying bias and the 0.001 percent increase is for trading tax
+        cash = int(0)
 
-    elif (price < df["ma_10"].iloc[i]) and buy == 1:
-        buy = 0
-        Cash = Shares * price 
-        Shares = 0
+    elif (price < df["ma_10"].iloc[i]) and buy == True:
+        buy = bool(False)
+        cash = shares * (df["Open"].iloc[i+1] * 0.999) #Sell on the next day to remove same day bias, -0.001 equals trading tax
+        shares = int(0)
 
 
 
-current_price = df["Close"].iloc[-1]
-Total_Value = Cash + (Shares * current_price)
-Profit = Total_Value - 1000
+current_price = df["Adj Close"].iloc[-1]
+Total_Value = float(cash + (shares * current_price))
+profit = float(Total_Value - 1000)
 
 print("Final Value: $" + str(round(Total_Value, 2)))
-print("Total Profit: $" + str(round(Profit, 2)))
-print("Percent Profit: %" + str(round((Profit / 1000) * 100, 2)))
+print("Total profit: $" + str(round(profit, 2)))
+print("Percent profit: %" + str(round((profit / 1000) * 100, 2)))
